635c01de4739c0375b9b4558fc747abe16c981c5
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-03-18 07:13:07 -0400

    Reduce database strategy to single record

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,32 +13,31 @@ const initdb = async () =>
   });
 
 
-export const putDb = async (content) => {
-  console.log('PUT to the database');
-  const jateDb = await openDB('jate', 1);
-  // Assign a transaction with jate with read/write privileges
-  // to tx making sure not to make extraneous async calls
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  // put the argument ('content') passed in a call in editor.js into the database as value
-  const request = store.put({ value: content });
-  // presumably id will autoincrement as per pre-supplied code
-  const result = await request;
-  console.log('🚀 - data saved to the database', result);
-};
-
-export const getDb = async () => {
-  console.log('GET all from the database');
-  const jateDb = await openDB('jate', 1);
-  // assign a transaction with jate with readwrite privs to tx as above
-  const tx = jateDb.transaction('jate', 'readwrite');
-  // obtain the object store via the transaction 
-  const store = tx.objectStore('jate');
-  const request = store.getAll();
-  const result = await request;
-  console.log('result.value', result);
-  return result;
-};
+  export const putDb = async (content) => {
+    console.log('PUT to the database');
+    const jateDb = await openDB('jate', 1);
+    // Assign a transaction with jate with read/write privileges
+    // to tx making sure not to make extraneous async calls
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+  // attempting to override the auto-increment in order always to obtain the entirety of the text
+    const request = store.put({ id: 0, value: content });
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+  };
+  
+  export const getDb = async () => {
+    console.log('GET text from the database');
+    const jateDb = await openDB('jate', 1);
+    // assign a transaction with jate with readonly privs to tx as above
+    const tx = jateDb.transaction('jate', 'readonly');
+    // obtain the object store via the transaction 
+    const store = tx.objectStore('jate');
+    const request = store.get(0);
+    const result = await request;
+    console.log('result.value', result.value);
+    return result.value;
+  };
 
 
 initdb();
