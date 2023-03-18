91779ba58e15b554aefbf8c566391de90db04dd8
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-03-18 05:29:00 -0400

    Add the database routes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,22 +12,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
+  // Assign a transaction with jate with read/write privileges
+  // to tx making sure not to make extraneous async calls
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 0, value: content });
+  // put the argument ('content') passed in a call in editor.js into the database as value
+  const request = store.put({ value: content });
+  // presumably id will autoincrement as per pre-supplied code
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
+  // assign a transaction with jate with readwrite privs to tx as above
   const tx = jateDb.transaction('jate', 'readwrite');
+  // obtain the object store via the transaction 
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
